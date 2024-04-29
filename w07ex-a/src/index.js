b55b8fb3797a5808c117c63014d224ccfca93087
unknown <itlects@gmail.com>
2024-04-29 13:51:20 +0900

    w07 start

---
+++ w07ex-a/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <h1>Hello React 7주차 공부!! </h1>
+
+);
+
